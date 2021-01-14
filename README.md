06639faea2f445953b38bc4219e9fcff96018a0f
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2021-01-14 16:54:55 +0530

    add run locally steps + comments

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ Simple command line todo list app built with Rust.
 - Mark a todo complete
 - Remove todo
 - Edit todo
+
+### Run Locally
+
+1. Make sure you have Rust and cargo installed: https://www.rust-lang.org/tools/install
+
+2. Clone this repository
+
+```
+git clone https://github.com/henryboisdequin/cli-todo.git
+cd cli-todo/
+```
+
+3. Run the app
+
+```
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -28,20 +28,24 @@ impl TodoList {
     }
 
     fn add_todo(&mut self) {
+        // read input
         println!("\nEnter your todo:\n");
         let mut name_of_todo = String::new();
         io::stdin().read_line(&mut name_of_todo).unwrap();
         name_of_todo = name_of_todo.split_whitespace().collect();
 
+        // push todo item
         let todo = TodoItem::new(name_of_todo, self.list.len());
         &self.list.push(todo);
     }
 
     fn print_todos(&self) {
+        // no todos
         if self.list.len() == 0 {
             println!("No todos.");
         }
 
+        // loop through and print all todos
         for item in &self.list {
             println!(
                 "Index: {} - [{}] - {}",
@@ -51,42 +55,52 @@ impl TodoList {
     }
 
     fn toggle_complete(&mut self) {
+        // read input
         println!("\nEnter the index of the todo you want to toggle complete:\n");
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let index: usize = input.trim().parse().expect("Please enter a number");
 
+        // set todo.completed to completed
         self.list[index].completed = 'x';
     }
 
     fn remove_todo(&mut self) {
+        // read input
         println!("\nEnter the index of the todo you want to remove:\n");
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let index: usize = input.trim().parse().expect("Please enter a number");
 
+        // remove todo from todo list
         self.list.remove(index);
     }
 
     fn edit_todo(&mut self) {
+        // read input
         println!("\nEnter the index of the todo you want to edit:\n");
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let index: usize = input.trim().parse().expect("Please enter a number");
 
+        // read edit todo name
         println!("\nEnter the name of the todo you want to update:\n");
         let mut name = String::new();
         io::stdin().read_line(&mut name).unwrap();
         name = name.trim().to_string();
 
+        // update the name
         self.list[index].name = name;
     }
 }
 
 fn main() {
+    // create a new todo list
     let mut todo_list = TodoList::new();
 
+    // loop forever
     loop {
+        // read command
         println!(
             "\nCommands:\n1: Get all todos\n2: Add a todo\n3: Toggle complete\n4: Remove todo\n5: Edit todo\n6: Quit\n"
         );
@@ -95,6 +109,7 @@ fn main() {
 
         command = command.trim().to_string();
 
+        // do action based on given command
         if command == "1" {
             todo_list.print_todos();
         } else if command == "2" {
